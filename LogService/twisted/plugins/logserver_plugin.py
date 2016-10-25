6d61f9bd4b6a2ae33a04b125ef1868c2a8d4fcb4
root <geert@greenitglobe.com>
2016-10-25 08:25:59 +0000

    Logservice v0.1

---
+++ LogService/twisted/plugins/logserver_plugin.py
@@ -0,0 +1,34 @@
+from zope.interface import implementer
+
+from twisted.python import usage
+from twisted.plugin import IPlugin
+from twisted.application.service import IServiceMaker
+from twisted.application import internet
+
+from logserver import LogServerFactory
+
+
+class Options(usage.Options):
+    optParameters = [["port", "p", 7422, "The port number to listen on."],
+                     ["path", "a", "/var/log/appscale/", "Path where logs are stored."],
+                     ["size", "s", 25, "Size in GiB of retention of logs."]]
+
+
+@implementer(IServiceMaker, IPlugin)
+class MyServiceMaker(object):
+    tapname = "myproject"
+    description = "Run this! It'll make your dog happy."
+    options = Options
+
+    def makeService(self, options):
+        """
+        Construct a TCPServer from a factory defined in myproject.
+        """
+        return internet.TCPServer(int(options["port"]), MyFactory())
+
+
+# Now construct an object which *provides* the relevant interfaces
+# The name of this variable is irrelevant, as long as there is *some*
+# name bound to a provider of IPlugin and IServiceMaker.
+
+serviceMaker = MyServiceMaker()
